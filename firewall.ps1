6cdab5ba007929d4be1b458331076f9980f8040a
sn0wflake <12665561+sn0wflake@users.noreply.github.com>
2019-05-02 03:16:30 +0200

    Add files via upload

---
+++ firewall.ps1
@@ -0,0 +1,22 @@
+#
+# firewall.ps1
+#
+# Get-NetFirewallRule >> x.txt to see what the empty paths are referencing.
+#
+
+ForEach($rule In Get-NetFirewallRule | Get-NetFirewallApplicationFilter) {
+    If(-not $rule.AppPath) { # UWP apps have no paths.
+        If($rule.CreationClassName.Substring(0, 22) -ne 'MSFT|FW|FirewallRule|{') { # Crude check for Windows 10 apps.
+            Write-Host 'REMOVING ID: ' $rule.InstanceID
+            Get-NetFirewallRule $rule.InstanceID | Remove-NetFirewallRule
+            #} else {
+            #Write-Host $rule.CreationClassName
+            #Get-NetFirewallRule $rule.InstanceID | Format-List Platform
+        }
+        } else { # Firewall rule contains a path.
+        If(-not (Test-Path $rule.AppPath) -and $rule.AppPath -ne 'System'-and $rule.AppPath.ToLower().IndexOf('\system32\svchost.exe') -eq -1) { # Crude check for Windows 10 itself.
+            Write-Host 'REMOVING APP:' $rule.AppPath
+            Get-NetFirewallRule $rule.InstanceID | Remove-NetFirewallRule
+        }
+    }
+}
